87c4acd91725790c3e0f8d610045362d93370186
Ilya Pleshakov <da-eto@ya.ru>
2015-08-06 23:09:37 +0300

    законченный конфиг gulp
    * очистка, билд, вотчер

--- gulpfile.js
+++ gulpfile.js
@@ -8,20 +8,18 @@ var gulp = require('gulp'),
     imagemin = require('gulp-imagemin'),
     rename = require('gulp-rename'),
     concat = require('gulp-concat'),
-    notify = require('gulp-notify'),
     cache = require('gulp-cache'),
     livereload = require('gulp-livereload'),
     del = require('del');
 
 // компилируем sass
 gulp.task('styles', function () {
-    return sass('app/static/sass/main.scss', {style: 'expanded'})
+    return sass('app/static/scss/main.scss', {style: 'expanded'})
         .pipe(autoprefixer('> 5%'))
         .pipe(gulp.dest('web/static/css'))
         .pipe(rename({suffix: '.min'}))
         .pipe(minifycss())
-        .pipe(gulp.dest('web/static/css'))
-        .pipe(notify({message: 'Styles task complete'}));
+        .pipe(gulp.dest('web/static/css'));
 });
 
 // компилируем js
@@ -37,14 +35,43 @@ gulp.task('scripts', function () {
         .pipe(gulp.dest('web/static/js'))
         .pipe(rename({suffix: '.min'}))
         .pipe(uglify())
-        .pipe(gulp.dest('web/static/js'))
-        .pipe(notify({message: 'Scripts task complete'}));
+        .pipe(gulp.dest('web/static/js'));
 });
 
 // минифицируем используемые изображения
 gulp.task('images', function () {
     return gulp.src('app/static/img/**/*')
         .pipe(cache(imagemin({optimizationLevel: 5, progressive: true, interlaced: true})))
-        .pipe(gulp.dest('web/static/img'))
-        .pipe(notify({message: 'Images task complete'}));
+        .pipe(gulp.dest('web/static/img'));
+});
+
+// очистка ассетов
+gulp.task('clean', function (cb) {
+    del(['web/static/css', 'web/static/js', 'web/static/img'], cb)
+});
+
+// сборка
+gulp.task('build', ['styles', 'scripts', 'images']);
+
+// полная пересборка
+gulp.task('rebuild', ['clean'], function () {
+    gulp.start('build');
+});
+
+// задача по умолчанию
+gulp.task('default', ['build']);
+
+// вотчер
+gulp.task('watch', function () {
+    // смотреть за стилями
+    gulp.watch('app/static/scss/**/*.scss', ['styles']);
+    // смотреть за скриптами
+    gulp.watch('app/static/js/**/*.js', ['scripts']);
+    // смотреть за изображениями
+    gulp.watch('app/static/img/**/*', ['images']);
+
+    // инстанс сервера LiveReload
+    livereload.listen();
+    // как только меняется что-то — сказать браузеру перегрузить страничку
+    gulp.watch(['web/static/**/*', 'web/*.html']).on('change', livereload.changed);
 });
