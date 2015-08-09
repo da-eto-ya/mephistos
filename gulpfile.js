c90165405a6a7db8bb95aea57bc26c698a467337
Ilya Pleshakov <da-eto@ya.ru>
2015-08-09 16:11:13 +0300

    добавляем компиляцию Materialize

--- gulpfile.js
+++ gulpfile.js
@@ -12,46 +12,109 @@ var gulp = require('gulp'),
     livereload = require('gulp-livereload'),
     del = require('del');
 
+// общий конфиг
+var conf = {
+    browsersTarget: '> 5%',
+    imagemin: {
+        optimizationLevel: 5,
+        progressive: true,
+        interlaced: true
+    },
+    paths: {
+        src: 'app/static/',
+        dest: 'web/static/',
+        materialize: 'lib/materialize-v0.97.0/',
+        html: 'web/**/*.html'
+    }
+};
+
 // компилируем sass
 gulp.task('styles', function () {
-    return sass('app/static/scss/main.scss', {style: 'expanded'})
-        .pipe(autoprefixer('> 5%'))
-        .pipe(gulp.dest('web/static/css'))
+    return sass(conf.paths.src + 'scss/main.scss', {
+        style: 'expanded',
+        container: 'gulp-ruby-sass-styles'
+    })
+        .pipe(autoprefixer(conf.browsersTarget))
+        .pipe(gulp.dest(conf.paths.dest + 'css'))
         .pipe(rename({suffix: '.min'}))
         .pipe(minifycss())
-        .pipe(gulp.dest('web/static/css'));
+        .pipe(gulp.dest(conf.paths.dest + 'css'));
 });
 
 // компилируем js
 gulp.task('scripts', function () {
     return gulp.src([
-        'app/static/js/some/**/*.js',
-        'app/static/js/other.js',
-        'app/static/js/main.js'
+        conf.paths.src + 'js/some/**/*.js',
+        conf.paths.src + 'js/other.js',
+        conf.paths.src + 'js/main.js'
     ])
         .pipe(jshint())
         .pipe(jshint.reporter('default'))
         .pipe(concat('main.js'))
-        .pipe(gulp.dest('web/static/js'))
+        .pipe(gulp.dest(conf.paths.dest + 'js'))
         .pipe(rename({suffix: '.min'}))
         .pipe(uglify())
-        .pipe(gulp.dest('web/static/js'));
+        .pipe(gulp.dest(conf.paths.dest + 'js'));
 });
 
 // минифицируем используемые изображения
 gulp.task('images', function () {
-    return gulp.src('app/static/img/**/*')
-        .pipe(cache(imagemin({optimizationLevel: 5, progressive: true, interlaced: true})))
-        .pipe(gulp.dest('web/static/img'));
+    return gulp.src(conf.paths.src + 'img/**/*')
+        .pipe(cache(imagemin(conf.imagemin)))
+        .pipe(gulp.dest(conf.paths.dest + 'img'));
 });
+
+// компилируем Materialize sass
+gulp.task('lib-materialize-css', function () {
+    return sass(conf.paths.src + conf.paths.materialize + 'sass/materialize.scss', {
+        style: 'expanded',
+        container: 'gulp-ruby-sass-materialize'
+    })
+        .pipe(autoprefixer(conf.browsersTarget))
+        .pipe(gulp.dest(conf.paths.dest + conf.paths.materialize + 'css'))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(minifycss())
+        .pipe(gulp.dest(conf.paths.dest + conf.paths.materialize + 'css'));
+});
+
+// компилируем (копируем) Materialize js
+gulp.task('lib-materialize-js', function () {
+    return gulp.src([
+        conf.paths.src + conf.paths.materialize + 'js/bin/*.js'
+    ])
+        .pipe(gulp.dest(conf.paths.dest + conf.paths.materialize + 'js'));
+});
+
+// копируем прочие Materialize assets
+gulp.task('lib-materialize-assets', function () {
+    return gulp.src([
+        conf.paths.src + conf.paths.materialize + 'font/**/*'
+    ])
+        .pipe(gulp.dest(conf.paths.dest + conf.paths.materialize + 'font'));
+});
+
+// целиком библиотека Materialize
+gulp.task('lib-materialize', [
+    'lib-materialize-css',
+    'lib-materialize-js',
+    'lib-materialize-assets'
+]);
+
+// все библиотеки
+gulp.task('libs', ['lib-materialize']);
 
 // очистка ассетов
 gulp.task('clean', function (cb) {
-    del(['web/static/css', 'web/static/js', 'web/static/img'], cb)
+    del([
+        conf.paths.dest + 'css',
+        conf.paths.dest + 'js',
+        conf.paths.dest + 'img',
+        conf.paths.dest + 'lib'
+    ], cb)
 });
 
 // сборка
-gulp.task('build', ['styles', 'scripts', 'images']);
+gulp.task('build', ['libs', 'styles', 'scripts', 'images']);
 
 // полная пересборка
 gulp.task('rebuild', ['clean'], function () {
@@ -63,15 +126,17 @@ gulp.task('default', ['build']);
 
 // вотчер
 gulp.task('watch', function () {
+    // смотреть за библиотеками
+    gulp.watch(conf.paths.src + 'lib/**/*', ['libs']);
     // смотреть за стилями
-    gulp.watch('app/static/scss/**/*.scss', ['styles']);
+    gulp.watch(conf.paths.src + 'scss/**/*.scss', ['styles']);
     // смотреть за скриптами
-    gulp.watch('app/static/js/**/*.js', ['scripts']);
+    gulp.watch(conf.paths.src + 'js/**/*.js', ['scripts']);
     // смотреть за изображениями
-    gulp.watch('app/static/img/**/*', ['images']);
+    gulp.watch(conf.paths.src + 'img/**/*', ['images']);
 
     // инстанс сервера LiveReload
     livereload.listen();
     // как только меняется что-то — сказать браузеру перегрузить страничку
-    gulp.watch(['web/static/**/*', 'web/*.html']).on('change', livereload.changed);
+    gulp.watch([conf.paths.dest + '**/*', conf.paths.html]).on('change', livereload.changed);
 });
