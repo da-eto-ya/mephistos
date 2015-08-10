1b82e28cc596c9c00a070d856eff6c056b43d7d0
Ilya Pleshakov <da-eto@ya.ru>
2015-08-10 13:15:12 +0300

    order form

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,6 +1,15 @@
 (function ($) {
-    // navigation
     $(function () {
+        // navigation
         $('.button-collapse').sideNav();
+
+        // counters
+        $(document).ready(function () {
+            $('textarea[maxlength]').each(function (idx, elem) {
+                var $elem = $(elem);
+                $elem.attr('length', $elem.attr('maxlength'));
+            });
+            $('textarea[length]').characterCounter();
+        });
     });
 })(jQuery);
