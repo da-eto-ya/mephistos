ceee43e8151c4cecedc75e2220ffb72e130916e4
Ilya Pleshakov <da-eto@ya.ru>
2015-08-11 13:53:59 +0300

    testing OpenShift getenv

--- web/index.php
+++ web/index.php
@@ -1,5 +1,8 @@
 <?php
 
-require_once __DIR__ . '/../app/app.php';
+//require_once __DIR__ . '/../app/app.php';
 
-app_start(require_once __DIR__ . '/../app/config.php');
+//app_start(require_once __DIR__ . '/../app/config.php');
+
+// testing OpenShift getenv
+var_dump(getenv('OPENSHIFT_MYSQL_DB_HOST'));
