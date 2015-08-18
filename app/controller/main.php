f5aebf4249bff2a4c994c168324fc37d5d7538b7
Ilya Pleshakov <da-eto@ya.ru>
2015-08-18 03:58:55 +0300

    redirect mainpage to default for authorized user

--- app/controller/main.php
+++ app/controller/main.php
@@ -11,5 +11,17 @@ require_services('request', 'response', 'template');
  */
 function controller_main()
 {
+    $user = auth_get_current_user();
+
+    if ($user) {
+        $path = auth_get_default_url_for_user($user);
+
+        if ($path && $path !== '/') {
+            response_redirect($path);
+
+            return;
+        }
+    }
+
     response_send(template_render('main'));
 }
