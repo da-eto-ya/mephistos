2db41d499a57b5e18bb08628d6480e098be117ce
Ilya Pleshakov <da-eto@ya.ru>
2015-08-18 20:54:43 +0300

    basic js validation for create order form

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -3,13 +3,30 @@
         // navigation
         $('.button-collapse').sideNav();
 
-        // counters
-        $(document).ready(function () {
-            $('textarea[maxlength]').each(function (idx, elem) {
-                var $elem = $(elem);
-                $elem.attr('length', $elem.attr('maxlength'));
-            });
-            $('textarea[length]').characterCounter();
-        });
+        // валидатор для непустых полей
+        $.validator.addMethod("notempty", function (value, element) {
+            return this.optional(element) || /\S/.test(value);
+        }, "Пожалуйста, укажите не пустое значение");
+
+        // значения по умолчанию
+        var defaults = {
+            validate: {
+                validClass: 'valid',
+                errorClass: 'invalid'
+            }
+        };
+
+        // форма добавления заказа
+        var $orderForm = $('#order-form');
+
+        if ($orderForm.length) {
+            $orderForm.validate($.extend({}, defaults.validate, {
+                rules: {
+                    description: {
+                        notempty: {}
+                    }
+                }
+            }));
+        }
     });
 })(jQuery);
